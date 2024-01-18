bfef601b861e8624c66edd9648afef53367daa10
Milan <milan.crayon@gmail.com>
2024-01-18 19:30:39 +0530

    Typo3 T3element Extension

---
+++ Classes/Controller/ElementsController.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+namespace Crayon\T3element\Controller; 
+
+class ElementsController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
+{   
+    /**
+     * action list
+     *
+     * @return string|object|null|void
+     */
+    public function indexAction()
+    {   
+
+    }
+}
