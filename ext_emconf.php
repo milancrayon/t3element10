8d792e2eb51b0e43c505be9d64566ba502e637ef
Milan <milan.crayon@gmail.com>
2024-02-02 11:48:55 +0530

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '10.4.0-10.4.12',
+            'typo3' => '10.4.0-10.4.99',
         ),
         'conflicts' => array(),
         'suggests' => array(), 
