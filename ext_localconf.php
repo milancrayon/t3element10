07e1caea6e366f84046b0663d5ed5dc0db3a314d
milancrayon <milan.crayon@gmail.com>
2024-11-21 12:16:11 +0530

    Update ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,4 +4,5 @@
    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:t3element/Configuration/TsConfig/Page/All.tsconfig">');
 
    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+   
 	$iconRegistry->registerIcon('t3element',\TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,['source' => 'EXT:t3element/Resources/Public/Icons/ic.png']);
